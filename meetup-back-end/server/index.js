c5470bf7c0dcd50b3bc520a7a2c6401342c06ee5
mta630 <michael.t.albert89@gmail.com>
2020-05-26 11:28:13 -0500

    Basic requirements completed

--- meetup-back-end/server/index.js
+++ meetup-back-end/server/index.js
@@ -3,17 +3,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const db = require('../db');
+const controllers = require('./controllers/attendee.js')
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/public')));
 
- 
-const dontUseMe = () => throw new Error('implement controllers');
-
-app.get('/attendees', dontUseMe);
-app.post('/attendees', dontUseMe);
+app.get('/attendees', (req, res) => {
+  controllers.getAll(req, res);
+});
+app.post('/attendees', (req, res) => {
+  controllers.add(req, res);
+});
 
 const PORT = process.env.PORT || 3000;
 
