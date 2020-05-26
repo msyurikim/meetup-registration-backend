a42794f920730c098de78f1f87e0a4f2a1bf2d9d
BHamp <brianscotthampton@gmail.com>
2020-05-26 12:18:22 -0400

    Utilize a carefully structured schema

--- meetup-back-end/db/models/Attendee.js
+++ meetup-back-end/db/models/Attendee.js
@@ -2,6 +2,17 @@ const mongoose = require('mongoose');
 
 const attendeeSchema = new mongoose.Schema({
   // your code here
+  firstName: STRING,
+  lastName: STRING,
+  email: STRING,
+  shirt: {
+    type: STRING,
+    enum: [`XS`, `S`, `M`, `L`, `XL`, `XXL`]
+  },
+  skillLevel: {
+    type: STRING,
+    enum: [`beginner`, `intermediate`, `expert`]
+  },,
 });
 
 const Attendee = mongoose.model('Attendee', attendeeSchema);
