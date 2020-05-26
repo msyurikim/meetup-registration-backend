f5c90a9b88dff5535a98e076600eba179bbd5b36
Justin Paoletta <justinpaoletta@gmail.com>
2020-05-26 12:57:33 -0400

    built out server models

--- meetup-back-end/server/controllers/attendee.js
+++ meetup-back-end/server/controllers/attendee.js
@@ -1,11 +1,25 @@
 const Attendee = require('../../db/models/Attendee');
 
+
 exports.getAll = (req, res) => {
   // your code here
-  
+  Attendee.find({}, (err, result) => {
+    if (err) {
+      res.send(500);
+    } else {
+      res.status(200).send(result);
+    }
+  })
 };
 
 exports.add = (req, res) => {
   // your code here
-
+  const newAttendee  = new Attendee(req.body);
+  newAttendee.save(req.body, (err, result) => {
+    if (err) {
+      res.send(500);
+    } else {
+      res.status(200).send(result);
+    }
+  })
 };
--- meetup-back-end/server/index.js
+++ meetup-back-end/server/index.js
@@ -2,18 +2,25 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const db = require('../db');
+const db = require('../db/models/attendee');
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/public')));
 
- 
-const dontUseMe = () => throw new Error('implement controllers');
 
-app.get('/attendees', dontUseMe);
-app.post('/attendees', dontUseMe);
+// const dontUseMe = () => throw new Error('implement controllers');
+
+// app.get('/attendees', dontUseMe);
+// app.post('/attendees', dontUseMe);
+
+app.get('/attendees', (req, res) => {
+
+});
+app.post('/attendees', (req, res) => {
+
+});
 
 const PORT = process.env.PORT || 3000;
 
