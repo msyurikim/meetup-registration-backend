ae3d58931665553175d79c8de72dbb606ac63ae4
Jeff Maughan <geophray@gmail.com>
2020-05-26 10:15:35 -0700

    Add getAll and add db methods

--- meetup-back-end/server/controllers/attendee.js
+++ meetup-back-end/server/controllers/attendee.js
@@ -1,9 +1,26 @@
 const Attendee = require('../../db/models/Attendee');
 
 exports.getAll = (req, res) => {
-  // your code here
+  Attendee.find({}, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Something went wrong!');
+    } else {
+      console.log(data);
+      res.status(200).send(data);
+    }
+  });
+
 };
 
 exports.add = (req, res) => {
-  // your code here
+  Attendee.create(req.body, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error creating new attendee.');
+    } else {
+      console.log(data);
+      res.status(201).send(data);
+    }
+  });
 };
