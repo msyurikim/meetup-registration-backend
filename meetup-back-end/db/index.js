a444dc51cbd24bfd8cdd1fac162e66fdc9d1e936
Sajjan Vaidya <sajjanrajvaidya@gmail.com>
2020-05-26 10:01:01 -0700

    Submit assessment:

--- meetup-back-end/db/index.js
+++ meetup-back-end/db/index.js
@@ -4,11 +4,43 @@ const mongoURI = 'mongodb://localhost:27017/jsmeetup';
 
 const db = mongoose.connect(mongoURI, { useNewUrlParser: true });
 
+const Attendee = require('./models/Attendee.js');
+
 db
   .then(db => console.log(`Connected to: ${mongoURI}`))
   .catch(err => {
     console.log(`There was a problem connecting to mongo at: ${mongoURI}`);
     console.log(err);
   });
+
+db.getAll = (query, cb) => {
+  Attendee.find({}, (err, results) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, results);
+    }
+  });
+};
+
+db.createOne = (query, cb) => {
+  const {firstName, lastName, email, shirt, skillLevel} = query;
+
+  var newDoc = new Attendee({
+  firstName,
+  lastName,
+  email,
+  shirt,
+  skillLevel,
+  });
+
+  Attendee.create(newDoc, (err, results) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, results);
+    }
+  })
+};
 
 module.exports = db;
