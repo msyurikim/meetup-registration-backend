3f477925da985067c4f58b578ffdf952acfd4ff9
Yuri Kim <yurikim123@gmail.com>
2020-05-26 07:50:13 -1000

    created controllers

--- meetup-back-end/server/controllers/attendee.js
+++ meetup-back-end/server/controllers/attendee.js
@@ -2,8 +2,23 @@ const Attendee = require('../../db/models/Attendee');
 
 exports.getAll = (req, res) => {
   // your code here
+  Attendee.find( (err,data) => {
+    if (err) {
+      res.status(400).end();
+    } else {
+      res.send(data);
+    }
+  });
 };
 
 exports.add = (req, res) => {
   // your code here
+  var data = req.body;
+  Attendee.create( data, (err, attendee) => {
+    if (err) {
+      res.status(400).end();
+    } else {
+      res.send(`${data.firstName} has been posted`);
+    }
+  });
 };
