a269132a0ef3057d4dcf675c2670f28f005a2f72
Jeff Maughan <geophray@gmail.com>
2020-05-26 09:25:19 -0700

    Build mongoose attendeeSchema

--- meetup-back-end/db/models/Attendee.js
+++ meetup-back-end/db/models/Attendee.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
 
 const attendeeSchema = new mongoose.Schema({
-  // your code here
+  firstName: String,
+  lastName: String,
+  email: String,
+  shirt: {
+    type: String,
+    enum: ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
+  },
+  skillLevel: {
+    type: String,
+    enum: ['beginner', 'intermediate', 'expert'],
+  },
 });
 
 const Attendee = mongoose.model('Attendee', attendeeSchema);
