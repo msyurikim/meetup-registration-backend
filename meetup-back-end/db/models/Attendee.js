1857482120a10a8eeb6674d721124bf20a071c16
Yuri Kim <yurikim123@gmail.com>
2020-05-26 07:49:02 -1000

    created schema for attendee

--- meetup-back-end/db/models/Attendee.js
+++ meetup-back-end/db/models/Attendee.js
@@ -2,6 +2,20 @@ const mongoose = require('mongoose');
 
 const attendeeSchema = new mongoose.Schema({
   // your code here
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: String,
+  email: String,
+  shirt: {
+    type: String,
+    enum: ['XS', 'S', 'M', 'L', 'XL']
+  },
+  skillLevel: {
+    type: String,
+    enum: ['beginner', 'intermediate', 'expert']
+  }
 });
 
 const Attendee = mongoose.model('Attendee', attendeeSchema);
