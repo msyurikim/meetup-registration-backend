554945fefa4e1921e8691d526286073021d92dbf
joshskkim <josh.sk.kim@gmail.com>
2020-05-26 09:27:25 -0700

    Rerouted server to use controllers

--- meetup-back-end/server/index.js
+++ meetup-back-end/server/index.js
@@ -9,11 +9,16 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/public')));
 
- 
+
 const dontUseMe = () => throw new Error('implement controllers');
 
-app.get('/attendees', dontUseMe);
-app.post('/attendees', dontUseMe);
+app.get('/attendees', (req, res) => {
+  db.getAll(req, res);
+});
+
+app.post('/attendees', (req, res) => {
+  db.add(req, res);
+});
 
 const PORT = process.env.PORT || 3000;
 
