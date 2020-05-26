1b122423c3e01d2cb20489fc1967c11849a893f4
Michael <griffinvmichael@gmail.com>
2020-05-26 09:28:02 -0700

    schema validation

--- meetup-back-end/db/models/Attendee.js
+++ meetup-back-end/db/models/Attendee.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 //TODO: Note: think about what types of constraints or validation these fields would benefit from
+//https://mongoosejs.com/docs/validation.html
+//Strings have enum, match, minlength, and maxlength validators.
 
 const attendeeSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   email: String,
-  shirt: String,
-  skillLevel: String
+  shirt: { type: String, enum: ["XS", "S", "M", "L", "XL", "XXL"] },
+  skillLevel: { type: String, enum: ["beginner", "intermediate", "expert"] }
 });
 
 const Attendee = mongoose.model('Attendee', attendeeSchema);
