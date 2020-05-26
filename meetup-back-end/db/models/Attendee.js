5064721e0cb5a224e57132fd9c4cbafa9597d7c9
Justin Paoletta <justinpaoletta@gmail.com>
2020-05-26 12:25:39 -0400

    schema complete

--- meetup-back-end/db/models/Attendee.js
+++ meetup-back-end/db/models/Attendee.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const attendeeSchema = new mongoose.Schema({
   // your code here
+  firstName: String,
+  lastName: String,
+  email: String,
+  shirt: [String],
+  skillLevel: [String],
 });
 
 const Attendee = mongoose.model('Attendee', attendeeSchema);
--- meetup-back-end/server/controllers/attendee.js
+++ meetup-back-end/server/controllers/attendee.js
@@ -2,8 +2,10 @@ const Attendee = require('../../db/models/Attendee');
 
 exports.getAll = (req, res) => {
   // your code here
+  
 };
 
 exports.add = (req, res) => {
   // your code here
+
 };
