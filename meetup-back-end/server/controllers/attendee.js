d6e233dffc79ce34762b7e07c744531151415148
JasperC1999 <jasperischauvin@gmail.com>
2020-05-26 12:24:53 -0500

    completed self assessment

--- meetup-back-end/server/controllers/attendee.js
+++ meetup-back-end/server/controllers/attendee.js
@@ -1,9 +1,42 @@
 const Attendee = require('../../db/models/Attendee');
 
 exports.getAll = (req, res) => {
-  // your code here
+  Attendee.find({}, (err, results) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(results);
+    }
+  })
 };
 
 exports.add = (req, res) => {
-  // your code here
+  console.log(req.body);
+  Attendee.create(req.body, (err, results) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(results);
+    }
+  });
+};
+
+exports.delete = (req,res) => {
+  Attendee.deleteOne(req.body, (err, results) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(results);
+    }
+  });
+};
+
+exports.update = (req,res) => {
+  Attendee.replaceOne(req.body.filter, req.body.doc, (err, results) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(results);
+    }
+  });
 };
