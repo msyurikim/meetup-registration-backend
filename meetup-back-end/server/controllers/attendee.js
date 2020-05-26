278ce2285bf28b59147226bcfa1367a2c5f39952
joshskkim <josh.sk.kim@gmail.com>
2020-05-26 09:19:23 -0700

    Added a get request

--- meetup-back-end/server/controllers/attendee.js
+++ meetup-back-end/server/controllers/attendee.js
@@ -2,6 +2,14 @@ const Attendee = require('../../db/models/Attendee');
 
 exports.getAll = (req, res) => {
   // your code here
+  Attendee.find({}).exec((err, data) => {
+    if (err) {
+      return res.status(400).send({
+        message: err,
+      });
+    }
+    return res.status(200).send(data);
+  })
 };
 
 exports.add = (req, res) => {
