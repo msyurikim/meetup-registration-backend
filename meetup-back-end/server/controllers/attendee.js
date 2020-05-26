06b38b49086bd67b091eef085b64bf30e25b3fa2
Jacob Orel <jakeorel@gmail.com>
2020-05-26 11:51:21 -0500

    filled out add and getAll functions

--- meetup-back-end/server/controllers/attendee.js
+++ meetup-back-end/server/controllers/attendee.js
@@ -1,9 +1,30 @@
 const Attendee = require('../../db/models/Attendee');
 
 exports.getAll = (req, res) => {
-  // your code here
+  Attendee.find((err, result) => {
+    if (err) {
+      res.sendStatus(404);
+    } else {
+      res.send(result);
+    }
+  });
 };
 
 exports.add = (req, res) => {
-  // your code here
+  const info = req.body;
+
+  const newAttendee = new Attendee({
+    firstName: info.firstName,
+    lastName: info.lastName,
+    email: info.email,
+    shirt: info.shirt,
+    skillLevel: info.skillLevel,
+  })
+  newAttendee.save(newAttendee, (err, result) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.send(result);
+    }
+  });
 };
